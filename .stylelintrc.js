9aa6505a91916ac834c835ed6b34df8bf296d7c9
coreyde <corey_239@hotmail.com>
2024-09-09 16:21:20 +1000

    init

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+export default {
+  extends: "stylelint-config-standard",
+  rules: {
+    indentation: 2,
+    "string-quotes": "double",
+    "color-hex-length": "short",
+    "max-empty-lines": 1,
+    "block-no-empty": true,
+    "declaration-colon-space-after": "always",
+  },
+};
